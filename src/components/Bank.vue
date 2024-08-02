ada818a7b09d1bde76bd73d65e05405615e339ca
ashley revlett <ashleyrevlett@gmail.com>
2024-08-02 13:10:57 -0400

    Fix

--- src/components/Bank.vue
+++ src/components/Bank.vue
@@ -10,14 +10,14 @@ const inventory = computed(() => bank.inventory);
 
 function buyStock() {
   const amount = 10;
-  const cost = market.price * amount;
+  const cost = market.getStock('AAPL')?.price! * amount;
   bank.withdraw(cost);
   bank.addInventory(amount);
 }
 
 function sellStock() {
   const amount = 10;
-  const cost = market.price * amount;
+  const cost = market.getStock('AAPL')?.price! * amount;
   bank.deposit(cost);
   bank.removeInventory(amount);
 }
@@ -32,8 +32,8 @@ function sellStock() {
     <div class="gap-2 flex flex-col">
       <button class="btn" @click="bank.deposit(10)">Deposit $10</button>
       <button class="btn" @click="bank.withdraw(10)">Withdraw $10</button>
-      <button class="btn" @click="buyStock">Buy 10 shares</button>
-      <button class="btn" @click="sellStock">Sell 10 shares</button>
+      <button class="btn" @click="buyStock">Buy 10 AAPL shares</button>
+      <button class="btn" @click="sellStock">Sell 10 AAPL shares</button>
     </div>
   </div>
 </template>
